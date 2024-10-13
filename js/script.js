206e100b79788717a680f52054af2dd0b47d33bd
YAMINISARIKI <21MH1A05C5@acoe.edu.in>
2024-10-13 14:20:16 +0530

    initial commit for new repository

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*==========typing animation=================*/
+var typed = new Typed(".typing",{
+    strings:["FullStack Trainee","Web Designer","web Developer"],
+    typeSpeed:100,
+    BackSpeed:100,
+    loop:true
+});
+
+
